2016697c7ee8ee25e021b78c1a23d9b2d47de954
TheTechnobear <mharris@technobear.com>
2025-03-28 18:30:31 +0100

    finalise beta setup for xmm

--- scripts/release.xmx.sh
+++ scripts/release.xmx.sh
@@ -1,9 +1,16 @@
 #/bin/sh
 echo XMX Release : copy percmd , resources and trax host to releases/xmx
+rm -rf releases/xmx
+mkdir releases/xmx
+
 cp ../PercCmd/releases/xmx/* releases/xmx/
+cp resources/*.txt releases/xmx/
 cp resources/xmx/* releases/xmx/
+
 cp build.xmx/TraxHost releases/xmx/
+cp build.xmx/rtaudio/librtaudio.so.7 releases/xmx/
 /opt/homebrew/bin/aarch64-elf-strip --strip-unneeded releases/xmx/TraxHost
+
 cd releases/xmx
 rm ../traxhost_xmx.zip
 zip -r ../traxhost_xmx.zip .
